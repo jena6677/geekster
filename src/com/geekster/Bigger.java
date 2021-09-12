326684f317414093dc36580f70f30d047d7b4203
Asutosh Jena <jenaasutosh2000@gmail.com>
2021-09-12 18:43:40 +0530

    bigger between two numbers

---
+++ src/com/geekster/Bigger.java
@@ -0,0 +1,12 @@
+package com.geekster;
+
+public class Bigger 
+{
+	public static void main(String[] args) 
+	{
+		int a = 45;
+		int b = 50;
+		int c = (a>b)?a:b;
+		System.out.println("bigger is:"+c);
+	}
+}
