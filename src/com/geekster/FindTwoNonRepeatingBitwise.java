b695091df59c936e6ff508484ef48e926bc14ae1
Asutosh Jena <jenaasutosh2000@gmail.com>
2021-10-02 19:35:06 +0530

    updated

--- src/com/geekster/FindTwoNonRepeatingBitwise.java
+++ src/com/geekster/FindTwoNonRepeatingBitwise.java
@@ -5,27 +5,22 @@ public class FindTwoNonRepeatingBitwise {
 	public static void main(String[] args) {
 		
 		int arr[] = {1,5,8,6,5,1,6,2};
-		int arr2[] = {1,2,4,8};
-		int start = 0, end = 3;
-		int sum = 0;
 		for(int i = 0; i < arr.length; i++)
 		{
-			sum ^= arr[i];
+			for(int j = i+1; j < arr.length; j++)
+			{
+				if((arr[i] ^ arr[j]) == 0)
+				{
+					arr[i] = 0;
+					arr[j] = 0;
+				}
+			}
 		}
-		while(end > start)
+		for(int i = 0; i < arr.length; i++)
 		{
-			if(arr2[start] + arr2[end] == sum)
+			if(arr[i] != 0)
 			{
-				System.out.println(arr2[start] +" "+arr2[end]);
-				break;
-			}
-			else if(arr2[start] + arr2[end] < sum)
-			{
-				start++;
-			}
-			else if(arr2[start] + arr2[end] > sum)
-			{
-				end--;
+				System.out.println(arr[i]);
 			}
 		}
 	}
