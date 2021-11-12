85b3c0b89b2eb6c4f07d6773eefe2d331f2e13c9
Asutosh Jena <jenaasutosh2000@gmail.com>
2021-11-12 19:22:05 +0530

    number frequency

--- src/com/geekster/dsa/NumberFrequency.java
+++ src/com/geekster/dsa/NumberFrequency.java
@@ -5,7 +5,7 @@ public class NumberFrequency {
 	public static void main(String[] args) {
 		
 		int arr[] = {1,1,2,2,4,5,5,6,6,6,6,9,10,11};
-		int target = 11;
+		int target = 6;
 		System.out.println(frequency(arr, target));
 	}
 	
